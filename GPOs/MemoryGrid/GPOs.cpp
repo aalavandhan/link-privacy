ef5200dd09d898e298f946f0eb7438e6328ecb4d
Nithin Krishna <nithinkrishh@gmail.com>
2017-06-24 03:44:40 -0700

    fixed vname

--- GPOs/MemoryGrid/GPOs.cpp
+++ GPOs/MemoryGrid/GPOs.cpp
@@ -1419,7 +1419,7 @@ void GPOs::groupLocationsToTopK(GPOs* gpos, set<int> *interested_checkins, int k
     delete candidates;
 
     if(seenLocations.size() % 100000==0)
-      cout << count << " " << endl;
+      cout << seenLocations.size() << " " << endl;
   }
 
   for(auto c_it = gpos->checkin_list.begin(); c_it != gpos->checkin_list.end(); c_it++){
